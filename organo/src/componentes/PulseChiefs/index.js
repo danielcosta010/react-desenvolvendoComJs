4f780e1cd5cbc55a38f60f5d790491e509e2e7d4
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2023-02-22 01:48:28 -0300

    Update index.js

--- organo/src/componentes/PulseChiefs/index.js
+++ organo/src/componentes/PulseChiefs/index.js
@@ -3,7 +3,9 @@ import './PulseChiefs.css'
 const Pulse = () => {
   return (
     <div className='pulse__chiefs'>
-      <img src='./img/logochiefs.png' className='pulse__img'/>
+      <a href="https://www.espn.com.br/nfl/time/_/nome/kc/kansas-city-chiefs" target='_blank' rel="noopener noreferrer">
+        <img src='./img/logochiefs.png' className='pulse__img' alt='logo do kansas city chiefs'/>
+      </a>
       <p className='pulse__titulo'>Kansas City Chiefs - Campeão do SuperBowl - <span className='pulse__titulo--go'>Go Chiiiieeefs</span></p>
     </div>
   )
